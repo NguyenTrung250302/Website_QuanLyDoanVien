65fd993e40df9e795c5674d653a6bcc5356e731c
Trung <nguyentrung250302@gmail.com>
2024-12-06 19:58:40 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const message = "number";
+const number = ref(10);
+const color = ref("color_red");
+const disable = ref(false);
+
+function change_color() {
+  color.value = "blue";
+  disable.value = true;
+  number.value = 4;
+}
+</script>
+
+<template>
+  <div>
+    <h1 :id="color">{{ message }} : {{ number }}</h1>
+    <button @click="change_color" :disabled="disable">click</button>
+  </div>
+</template>
+
+<style scoped>
+#color_red {
+  color: rgb(255, 0, 0);
+}
+
+#blue {
+  color: aqua;
+}
+</style>
